19dfbbecd3ee5103f4ba07ae0a1cfa7fe2f1e695
Oswaldo Orellana <oswaldo.orellana.v@gmail.com>
2024-10-08 11:37:40 -0400

    redirect auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
 import styles from './page.module.css'
+import session from './../node_modules/next-auth/core/routes/session.d';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+
+export default async function Home() {
+  const session = await getServerSession();
+  
+  if(!session){
+    redirect('/api/auth/signin');
+  }
+
+
   return (
     <main>
       
